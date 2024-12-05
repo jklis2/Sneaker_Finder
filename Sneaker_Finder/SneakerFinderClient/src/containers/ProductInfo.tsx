1a4d629f4acbb48753f5a30b44e3c457cb2c0f3b
RobertSternal <robst@onet.pl>
2024-12-06 00:56:36 +0100

    feat: Add id passing in ProductInfo component

--- Sneaker_Finder/SneakerFinderClient/src/containers/ProductInfo.tsx
+++ Sneaker_Finder/SneakerFinderClient/src/containers/ProductInfo.tsx
@@ -1,10 +1,11 @@
 import ProductInfoComp from "../components/ProductInfoComp";
 
 interface ProductInfoProps {
+  id: string;
   name: string;
   price: number;
 }
 
-export default function ProductInfo({ name, price }: ProductInfoProps) {
-  return <ProductInfoComp name={name} price={price} retail={price * 0.7} />;
+export default function ProductInfo({ id, name, price }: ProductInfoProps) {
+  return <ProductInfoComp id={id} name={name} price={price} retail={price * 0.7} />;
 }
