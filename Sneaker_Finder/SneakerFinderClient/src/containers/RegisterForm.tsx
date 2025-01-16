7ce60fc747ec439b721bcd6f670d3153dae2cbef
RobertSternal <robst@onet.pl>
2025-01-16 22:46:34 +0100

    feat: Update changing routes for register endpoint

--- Sneaker_Finder/SneakerFinderClient/src/containers/RegisterForm.tsx
+++ Sneaker_Finder/SneakerFinderClient/src/containers/RegisterForm.tsx
@@ -65,7 +65,7 @@ export default function RegisterForm() {
     }
 
     try {
-      const response = await fetch(`${API_URL}/api/auth/register`, {
+      const response = await fetch(`${API_URL}/api/users/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
